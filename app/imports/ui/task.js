c0c436ffe658b874a6140b667ca24e692d7f0185
Jipeng_Huang <jipenghuang@dhcp-168-105-105-181.wireless.manoa.hawaii.edu>
2016-10-10 20:23:26 -1000

    Task 3: Collections, Forms and events, update and remove

--- app/imports/ui/task.js
+++ app/imports/ui/task.js
@@ -1,3 +1,17 @@
-/**
- * Created by jipenghuang on 10/10/16.
- */
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.html';
+
+Template.task.events({
+  'click .toggle-checked'() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this._id, {
+      $set: { checked: ! this.checked },
+    });
+  },
+  'click .delete'() {
+    Tasks.remove(this._id);
+  },
+});
